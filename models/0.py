40fa63496ff2a89cdbbedb23dfcb61825339be79
Gary Bustillos <g_a_b_m@msn.com>
2011-11-06 16:46:49 -0430

    inicio del proyecto YAYY

---
+++ models/0.py
@@ -0,0 +1,19 @@
+from gluon.storage import Storage
+settings = Storage()
+
+settings.migrate = True
+settings.title = 'Casa de Apuestas'
+settings.subtitle = ''
+settings.author = 'JSG Systems'
+settings.author_email = 'jsg@example.com'
+settings.keywords = 'casa, apuestas'
+settings.description = ''
+settings.layout_theme = 'Balanced'
+settings.database_uri = 'sqlite://storage.sqlite'
+settings.security_key = 'd5ef8126-550e-491a-aa42-75a0f4a49c6f'
+settings.email_server = 'localhost'
+settings.email_sender = 'you@example.com'
+settings.email_login = ''
+settings.login_method = 'local'
+settings.login_config = ''
+settings.plugins = []
