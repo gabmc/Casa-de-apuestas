415ac84f1a3271a52564bc20141e33e3ad280032
jperozo <juanfran_121@hotmail.com>
2011-11-13 17:44:04 -0430

    default.py, sql.log, storage.sqlite, db.py cambiados

--- controllers/default.py
+++ controllers/default.py
@@ -4,6 +4,8 @@ def user(): return dict(form=auth())
 def download(): return response.download(request,db)
 def call(): return service()
 ### end requires
+
+@auth.requires_login()
 def index():
     return dict()
 
