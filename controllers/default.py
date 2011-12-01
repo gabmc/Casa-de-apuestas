44b10d9a499a3c310245f2f8160fd652764d5ddc
jperozo <juanfran_121@hotmail.com>
2011-11-30 22:37:53 -0430

    default.py, metodo mostrar()

--- controllers/default.py
+++ controllers/default.py
@@ -102,9 +102,10 @@ def mostrar():
     dao2 = DAOCasadeApuestas()
     dao3 = DAOCasadeApuestas()
     dao4 = DAOCasadeApuestas()
-    archivo = dao.generarArchivoActualizacion(dao.generarMensajeMD5(db().select(db.eventos.ALL)), dao2.generarMensajeMD5(db().select(db.categorias.ALL)), dao3.generarMensajeMD5(db().select(db.participantes.ALL)), dao4.generarMensajeMD5(db().select(db.eventosparticipantes.ALL)))
-    return dict(eventosMD5 = dao.generarMensajeMD5(db().select(db.eventos.ALL)), categoriasMD5 = dao.generarMensajeMD5(db().select(db.categorias.ALL)), participantesMD5 = dao.generarMensajeMD5(db().select(db.participantes.ALL)), eventosparticipantesMD5 = dao.generarMensajeMD5(db().select(db.eventosparticipantes.ALL)), eventos = dao.generarFormatoXml(db().select(db.eventos.ALL)), categorias = dao.generarFormatoXml(db().select(db.categorias.ALL)), archivo = archivo)
-    
+    dao5 = DAOCasadeApuestas()
+    q = dao.generarArchivoActualizacionMD5(dao.generarMensajeMD5(db().select(db.eventos.ALL)), dao2.generarMensajeMD5(db().select(db.categorias.ALL)), dao3.generarMensajeMD5(db().select(db.participantes.ALL)), dao4.generarMensajeMD5(db().select(db.eventosparticipantes.ALL)), dao5.generarMensajeMD5(db().select(db.auth_user.ALL)))
+    s = dao.generarArchivoActualizacionXML(dao.generarFormatoXml(db().select(db.eventos.ALL)), dao2.generarFormatoXml(db().select(db.categorias.ALL)), dao3.generarFormatoXml(db().select(db.participantes.ALL)), dao4.generarFormatoXml(db().select(db.eventosparticipantes.ALL)), dao5.generarFormatoXml(db().select(db.auth_user.ALL)))
+    return dict(q = q, s = s)   
     
 def verProximosEventos1():
     import datetime
