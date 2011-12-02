5557c30e6f48c0dc19461d2665ed98d397bc9b19
jperozo <juanfran_121@hotmail.com>
2011-12-02 02:07:02 -0430

    Documentacion Log

--- modules/log.py
+++ modules/log.py
@@ -5,9 +5,19 @@ from datetime import *
 from time import *
 
 class log:
+    """
+    Constructor de la clase log.
+    Se asigna un directorio de ubicacion del archivo
+    """
     def __init__(self):
         self.__path = "C:/Python32/web2py_win/web2py/applications/Casa_de_Apuestas/static/bitacora.log"
 
+    """
+    Metodo encargado de abrir y escribir en el archivo. 
+    En caso de no existir, lo crea
+    
+    @param info: Informacion que se va a guardar en la bitacora
+    """
     def logear(self, info):
         try:
             f = open(self.__path, "a")
