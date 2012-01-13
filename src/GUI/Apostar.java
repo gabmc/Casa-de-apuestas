d6cf2ce61b03ac79b51b3a924311ad2760d76a9a
Hector Sam <hfsam88@gmail.com>
2012-01-13 11:25:44 -0430

    arreglado apostar

--- src/GUI/Apostar.java
+++ src/GUI/Apostar.java
@@ -73,7 +73,8 @@ public class Apostar extends javax.swing.JFrame {
     }// </editor-fold>//GEN-END:initComponents
 
     private void jButton1ActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_jButton1ActionPerformed
-        new InicioCategoria().setVisible(true);
+  //      new InicioCategoria("FALSE").setVisible(true);
+        Persistencia.HiloUSB.dti.setVentana(new InicioCategoria());
         this.dispose();
     }//GEN-LAST:event_jButton1ActionPerformed
 
